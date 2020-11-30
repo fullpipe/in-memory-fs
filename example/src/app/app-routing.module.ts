1d2b2905dd0191362d6fbe40fce167d50549080b
Eugene Bravov <eugene.bravov@gmail.com>
2020-12-01 00:28:48 +0300

    Add better example

---
+++ example/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
+import { CComponent } from './c/c.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AComponent
+  },
+  {
+    path: 'b',
+    component: BComponent
+  },
+  {
+    path: 'c',
+    component: CComponent
+  },
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
